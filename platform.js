b0c8fa1ed9a19d6a4053b7f672425c8911c74adb
Milton Ngan <milton@valvesoftware.com>
2020-09-27 17:30:41 -0700

    New door sense feature

--- platform.js
+++ platform.js
@@ -59,7 +59,7 @@ class AugustPlatform {
   
     
   
-    // Method to setup accesories from config.json
+    // Method to setup accessories from config.json
     didFinishLaunching() {
   
       if ((this.email || this.phone) && this.password) {
@@ -135,6 +135,11 @@ class AugustPlatform {
         .on('get', this.getState.bind(this, accessory))
         .on('set', this.setState.bind(this, accessory));
   
+      accessory
+        .getService(this.Service.ContactSensor)
+        .getCharacteristic(this.Characteristic.ContactSensorState)
+        .on('get', this.getDoorState.bind(this, accessory));
+
     //   accessory
     //     .getService(this.Service.BatteryService)
     //     .getCharacteristic(this.Characteristic.BatteryLevel);
@@ -198,7 +203,13 @@ class AugustPlatform {
       callback(null, accessory.context.currentState);
   
     }
-  
+
+    // Method to get target door state
+    getDoorState(accessory, callback) {
+      // Get target state directly from cache
+      callback(null, accessory.context.doorState);
+    }
+
     // Method for state periodic update
     periodicUpdate() {
       var self = this;
@@ -256,6 +267,10 @@ class AugustPlatform {
         .getCharacteristic(this.Characteristic.LockTargetState)
         .getValue();
   
+      accessory
+        .getService(this.Service.ContactSensor)
+        .setCharacteristic(this.Characteristic.ContactSensorState, accessory.context.doorState);
+
     //   accessory
     //     .getService(this.Service.BatteryService)
     //     .setCharacteristic(this.Characteristic.BatteryLevel, accessory.context.batt);
@@ -388,34 +403,30 @@ class AugustPlatform {
         var thisModel = locks.lockType.toString();
         var thislockName = lockName;
         var state = (lock.status == "kAugLockState_Locked") ? "locked" : (lock.status == "kAugLockState_Unlocked") ? "unlocked" : "error";
-        var nameFound = true;
-        var stateFound = true;
+        var doorState = (lock.doorState == 'kAugDoorState_Closed') ? "closed" : (lock.doorState == 'kAugDoorState_Open') ? "open": "unknown";
+        var isDoorOpened = doorState == "open" ? 1 : 0;
         var thishome = houseName;
         // battery no longer provided over api calls
         self.batt = 100;
   
-        var locked = state == "locked";
-        var unlocked = state == "unlocked";
-  
-        var thislockState = (state == "locked") ? "1" : "0";
         var isStateChanged = false;
 
-        if (self.batt < 20) {
-          var lowbatt = self.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
-          var newbatt = self.Characteristic.LockCurrentState.SECURED;
+        // if (self.batt < 20) {
+        //   var lowbatt = self.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
+        //   var newbatt = self.Characteristic.LockCurrentState.SECURED;
   
-        } else if (self.batt > 20) {
-          var lowbatt = self.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
-          var newbatt = self.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+        // } else if (self.batt > 20) {
+        //   var lowbatt = self.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+        //   var newbatt = self.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
   
-        }
+        // }
   
         // Initialization for opener
         if (!self.accessories[thisDeviceID]) {
           var uuid = self.UUIDGen.generate(thisDeviceID);
-            var _Accesory = self.Accessory;
+            var _Accessory = self.Accessory;
           // Setup accessory as GARAGE_lock_OPENER (4) category.
-          var newAccessory = new _Accesory("August " + thishome, uuid, 6);
+          var newAccessory = new _Accessory("August " + thislockName, uuid, 6);
   
           // New accessory found in the server is always reachable
           newAccessory.reachable = true;
@@ -429,18 +440,20 @@ class AugustPlatform {
           newAccessory.context.model = thisModel;
         // newAccessory.context.batt = self.batt;
           newAccessory.context.low = self.low;
+          newAccessory.context.doorState = isDoorOpened;
   
           newAccessory.context.log = function (msg) { self.log("[" + newAccessory.displayName + "]", msg); };
   
           // Setup HomeKit security systemLoc service
           newAccessory.addService(self.Service.LockMechanism, thislockName);
+          newAccessory.addService(self.Service.ContactSensor, thislockName);
           //newAccessory.addService(this.Service.BatteryService);
           // Setup HomeKit accessory information
           self.setAccessoryInfo(newAccessory);
           // Setup listeners for different security system events
           self.setService(newAccessory);
           // Register accessory in HomeKit
-          self.platformLog("adding lock lock to homebridge");
+          newAccessory.context.log("Adding lock lock to homebridge");
           self.api.registerPlatformAccessories("homebridge-august-smart-locks", "AugustLocks", [newAccessory]);
           isStateChanged = true;
 
@@ -483,7 +496,13 @@ class AugustPlatform {
           }
   
         }
+
+        if (isDoorOpened != newAccessory.context.doorState) {
+          isStateChanged = true;
+          newAccessory.context.doorState = isDoorOpened;
   
+        }
+
         // Store accessory in cache
         self.accessories[thisDeviceID] = newAccessory;
   
@@ -517,9 +536,8 @@ class AugustPlatform {
       var self = this;
       var lockCtx = accessory.context;
       var status = this.lockState[state];
-      var augustState = (state == self.Characteristic.LockTargetState.SECURED) ? this.augustApi.lock(lockCtx.deviceID) : this.augustApi.unlock(lockCtx.deviceID);
+      var remoteOperate = (state == self.Characteristic.LockTargetState.SECURED) ? this.augustApi.lock(lockCtx.deviceID) : this.augustApi.unlock(lockCtx.deviceID);
   
-      var remoteOperate = this.augustApi.lock(lockCtx.deviceID);
       remoteOperate.then(function (result) {
         lockCtx.log("State was successfully set to " + status);
   
